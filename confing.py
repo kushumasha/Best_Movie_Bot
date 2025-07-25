93720dbfa05e9528d4b3600288e0dfdeddaca2c3
kushumasha <skr346902@gmail.com>
2025-07-25 18:26:06 +0530

    confing.py

---
+++ confing.py
@@ -0,0 +1,4 @@
+BOT_TOKEN = "your_bot_token_here"
+API_ID = 12345678  # Replace with your Telegram API ID
+API_HASH = "your_api_hash"  # Replace with your Telegram API HASH
+CHANNEL_USERNAME = "your_channel_username"  # Without @
