10aefd3d98d28b5f395d870112babb624856f21f
kushumasha <skr346902@gmail.com>
2025-07-25 20:05:37 +0530

    Update confing.py

--- confing.py
+++ confing.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "your_bot_token_here"
-API_ID = 12345678  # Replace with your Telegram API ID
-API_HASH = "your_api_hash"  # Replace with your Telegram API HASH
-CHANNEL_USERNAME = "your_channel_username"  # Without @
+BOT_TOKEN = "7304807538:AAGWu2g3ZTtofbUJTdRs9V4aWeYuwvgtzhQ"
+API_ID = 14689508  # Replace with your Telegram API ID
+API_HASH = "79413cfe2d8cc93ddf1815ef588e80d5"  # Replace with your Telegram API HASH
+CHANNEL_USERNAME = "-1002738918719"  # Without @
